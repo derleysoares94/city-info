367e328b2d47f8da4d3800e55fd3a53708e12612
Wanderley Soares Feitosa <wanderleysoares1994@gmail.com>
2024-08-02 18:57:09 +0100

    Calling weather API  and displaying weather inf

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,38 +2,62 @@ const search = document.getElementById('search');
 
 search.addEventListener('click', async (event) => {
     event.preventDefault();
-    const cityName = document.getElementById('city').value;
-    const inf = document.getElementById('inf');
-    const loader = document.getElementById('loader');
+    const cityName          = document.getElementById('city').value;
+    const inf               = document.getElementById('inf');
+    const temp              = document.getElementById('temperature');
+    const img               = document.getElementById('wheater-img');
+    const current_weather   = document.getElementById('cur-weather');
+    const real_feel         = document.getElementById('real-feel');
+    const wind              = document.getElementById('wind');
+    const loader            = document.getElementById('loader');
+    const weather_container = document.getElementById('weather-container');
 
     inf.innerHTML = '';
     loader.classList.remove("hide-loader");
+    weather_container.style.display = "none";
 
-    const result = await getHistory(cityName);
+    const cityInfo = await getCityInfo(cityName);
 
-    //Adding the historial text to the info container.
-    if(result.length !== 0) {
-        result.forEach(history => {
+    const weather     = await getWheater(cityInfo[0].latitude, cityInfo[0].longitude);
+    const icon        = weather.weather[0].icon;
+    const desc        = weather.weather[0].description;
+    const feels_like  = weather.main.feels_like;
+    const wind_speed  = weather.wind.speed;
+    const temperature = weather.main.temp;
+
+    temp.innerText            = temperature;
+    img.src                   = `/resources/img/wheater-icon/${icon}.png`;
+    current_weather.innerText = desc;
+    real_feel.innerText       = feels_like;
+    wind.innerText            = wind_speed;
+
+    const histories = await getHistory(cityName);
+
+    if (histories.length !== 0) {
+        histories.forEach(history => {
             let text = `${history.day}/${history.month}/${history.year} Event: ${history.event}` + "<br>";
-            loader.classList.add("hide-loader");
             inf.innerHTML += text + "<br>";
             //typingText(text, document.getElementById('inf'));
         });
+        loader.classList.add("hide-loader");
+        weather_container.style.display = "flex";
+        
     } else {
         loader.classList.add("hide-loader");
         inf.innerHTML = `Sorry, we could not find any history event for ${cityName}`;
+        weather_container.style.display = "flex";
     }
 });
 
-//Get the cities info, its necessary to fetch the weather forecast.
-async function getCityInf(city) {    
+//Get the cities info, to be possible use the free version of the API we need to get the latitude and longitude.
+async function getCityInfo(city) {
     const url = `https://api.api-ninjas.com/v1/city?name=${city}`;
     const result = await fetch(url,
     {
         method: 'GET',
         headers: { 'X-Api-Key': 'SxHy1r0kNeQbdwEnBAUDeQ==XiZ38nUgTDZrQmc8' },
     })
-    .then(response => {response.json()})
+    .then(response => response.json())
     .then(data => { return data })
     .catch(error => console.error(error));    
     
@@ -49,11 +73,23 @@ async function getHistory(city) {
         headers: {'X-Api-Key': 'SxHy1r0kNeQbdwEnBAUDeQ==XiZ38nUgTDZrQmc8'},
     })
     .then(response => response.json())
-    .then(data => { 
-        console.log(data); 
-        return data; 
-    })
+    .then(data => { return data; })
     .catch(error => console.error(error));
+
+    return result;
+}
+
+//Get wheater forecast of the city using latitude and longitude.
+async function getWheater(lat, lon) {
+    const key = '115f8ede73325a9df6cd7c03106ebadd';
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`;
+    const result = await fetch(url,
+        {
+            method: 'GET',
+        })
+        .then(response => response.json())
+        .then(data => { return data })
+        .catch(error => console.error(error));
 
     return result;
 }
